d0de9d34da6e6ed12a9e8344dccbeaa63bd65d32
Jarosław Dutka <jaroslawjdutka@gmail.com>
2020-05-06 14:36:09 +0200

    Rename executable to devtools

--- DevTools/Scripts/dt-setup.cmd
+++ DevTools/Scripts/dt-setup.cmd
@@ -1,6 +1,6 @@
 @echo off
 set PATH=%PATH%;%~dp0
 set BASEPATH=%PATH%
-call %~dp0\..\dt.exe path > "%~dp0\..\path.txt"
+call %~dp0\..\devtools.exe path > "%~dp0\..\path.txt"
 set /p DEVPATH=<"%~dp0\..\path.txt"
 set PATH=%BASEPATH%;%DEVPATH%
--- DevTools/Scripts/dt.cmd
+++ DevTools/Scripts/dt.cmd
@@ -1,5 +1,5 @@
 @echo off
-call %~dp0\..\dt.exe %*
-call %~dp0\..\dt.exe path > "%~dp0\..\path.txt"
+call %~dp0\..\devtools.exe %*
+call %~dp0\..\devtools.exe path > "%~dp0\..\path.txt"
 set /p DEVPATH=<"%~dp0\..\path.txt"
 set PATH=%BASEPATH%;%DEVPATH%
