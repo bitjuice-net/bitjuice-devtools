b79bd115d41fcce93f6093b5633cc18e277993a5
Jarosław Dutka <jaroslawjdutka@gmail.com>
2021-12-14 15:17:42 +0100

    Prevent multiple setups

--- DevTools/Scripts/dt-setup.cmd
+++ DevTools/Scripts/dt-setup.cmd
@@ -1,5 +1,9 @@
 @echo off
 
+if defined DT_INITIALIZED (
+	goto exit
+)
+
 set PATH=%PATH%;%~dp0
 set DT_BASEPATH=%PATH%
 
@@ -8,3 +12,6 @@ call %~dp0\..\devtools.exe setup > "%DT_SETUP_FILENAME%"
 
 for /f "tokens=*" %%i in (%DT_SETUP_FILENAME%) do set %%i
 set PATH=%DT_BASEPATH%;%DT_PATH%
+set DT_INITIALIZED=true
+
+:exit
